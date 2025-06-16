8e846ab13dcefdc9c683169bebe980e6ae72c10c
Tanto23 <anthonyofemdu@gmail.com>
2025-06-16 17:17:40 +0100

    frist commit

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+require("./config/database")
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.listen(PORT, () =>{
+  console.log(`Server is running on port ${PORT}`);
+})
